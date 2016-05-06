eddf28596b5f54a13647a4572fc53c7daa9a5c77
michalpukf <michal.pagac@student.ukf.sk>
2016-05-06 21:25:46 +0200

    Pridanie skrolovacej animacie

---
+++ js/index.js
@@ -0,0 +1,21 @@
+(function($) {
+    var navbar = $('.navbar-nav');
+    var navLink = navbar.find('a');
+    var menu = $('.navbar-brand');
+    navLink.on('click', function(event) {
+        console.log("Hello");
+
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top - 50
+        }, 1000, "easeInOutExpo");
+        event.preventDefault();
+    });
+
+    menu.on('click', function(event) {
+        $('html, body').animate({
+            scrollTop: 0
+        });
+        event.preventDefault();
+    });
+
+})(jQuery);
